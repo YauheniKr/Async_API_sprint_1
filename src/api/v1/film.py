538fca659d68ff061ce9b805ea641a8549d6eb35
Yauheni <smilodon2@yandex.ru>
2021-11-22 02:01:13 +0300

    Depends зависит от get_film_service, поправил импорты добавил проверку на наличие результата запроса, если ничего не получили возвращаем 404 добавил обработку запрсоа поиска по фильмам удалил комментарии

--- src/api/v1/film.py
+++ src/api/v1/film.py
@@ -4,44 +4,38 @@ from typing import List, Optional
 from fastapi import APIRouter, Depends, HTTPException, Query
 
 from models.film import BaseFilm, FullFilm
-from services.film import FilmService
+from services.film import FilmService, get_film_service
 
 router = APIRouter()
 
 
 @router.get('/', response_model=List[BaseFilm])
 async def get_films(
-        sort: str, film_service: FilmService = Depends(), page_number=Query(default=1, alias='page[number]'),
-        size=Query(default=50, alias='page[size]'), filter_request: Optional[str] = Query(None, alias='filter[genre]')):
+        sort: str, film_service: FilmService = Depends(get_film_service),
+        page_number=Query(default=1, alias='page[number]'), size=Query(default=50, alias='page[size]'),
+        filter_request: Optional[str] = Query(None, alias='filter[genre]')):
     if 'imdb_rating' in sort:
         sort = sort.replace('imdb_rating', 'rating')
     films = await film_service._get_film_list_from_elastic(sort=sort, page_number=page_number, size=size,
                                                            filter_request=filter_request)
+    if not films:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
     return films
 
 
 @router.get('/search', response_model=List[BaseFilm])
-async def search_films(
-        query: str, page_number=Query(default=1, alias='page[number]'), size=Query(default=50, alias='page[size]')
-):
-    pass
+async def search_films(query: str, film_service: FilmService = Depends(get_film_service),
+                       page_number=Query(default=1, alias='page[number]'), size=Query(default=50, alias='page[size]')
+                       ):
+    films = await film_service._search_film_in_elastic(query=query, page_number=page_number, size=size)
+    if not films:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
+    return films
 
 
-# Внедряем FilmService с помощью Depends(get_film_service)
-# TODO тут response_model должен быть FullFilm
 @router.get('/{film_id}', response_model=FullFilm)
-async def film_details(film_id: str, film_service: FilmService = Depends()) -> FullFilm:
+async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> FullFilm:
     film = await film_service.get_by_id(film_id)
     if not film:
-        # Если фильм не найден, отдаём 404 статус
-        # Желательно пользоваться уже определёнными HTTP-статусами, которые содержат enum
-        # Такой код будет более поддерживаемым
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
-
-    # Перекладываем данные из models.Film в Film
-    # Обратите внимание, что у модели бизнес-логики есть поле description
-    # Которое отсутствует в модели ответа API.
-    # Если бы использовалась общая модель для бизнес-логики и формирования ответов API
-    # вы бы предоставляли клиентам данные, которые им не нужны
-    # и, возможно, данные, которые опасно возвращать
     return film
